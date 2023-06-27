3131f81731a9752d60fb2baec3a2cdb9461ab53a
anorak <theanorak@protonmail.com>
2023-06-27 10:34:24 -0500

    Camvbios finales

--- Objetos/jugador/jugador.gd
+++ Objetos/jugador/jugador.gd
@@ -1,10 +1,4 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+@export var ID: int
+@export var color : Color
